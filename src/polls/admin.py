59fc9f4d34d939c73f43a6312fd543b577d83607
qiangjun <zhangqj616@gmail.com>
2020-08-27 10:39:16 +0100

    add CSS styles and background image

--- src/polls/admin.py
+++ src/polls/admin.py
@@ -4,5 +4,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
